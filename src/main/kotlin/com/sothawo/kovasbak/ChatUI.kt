7e5dfe414d3273ea7a972e2c0c05b4f4dfc8f94f
P.J.Meisch <pj.meisch@sothawo.com>
2017-07-02 13:36:22 +0200

    put the UI classes in one source file

--- src/main/kotlin/com/sothawo/kovasbak/ChatUI.kt
+++ src/main/kotlin/com/sothawo/kovasbak/ChatUI.kt
@@ -3,6 +3,7 @@ package com.sothawo.kovasbak
 import com.vaadin.event.ShortcutAction
 import com.vaadin.server.Sizeable
 import com.vaadin.server.VaadinRequest
+import com.vaadin.shared.ui.ContentMode
 import com.vaadin.spring.annotation.SpringUI
 import com.vaadin.ui.*
 import org.slf4j.Logger
@@ -16,10 +17,6 @@ class ChatUI : UI() {
     lateinit var user: String
     lateinit var chatDisplay: ChatDisplay
 
-    /**
-     * called to initialize the UI. adds the chat display and the input controls and asks for the user's name.
-     * @param vaadinRequest ignored here
-     */
     override fun init(vaadinRequest: VaadinRequest?) {
         content = VerticalLayout().apply {
             setSizeFull()
@@ -30,9 +27,6 @@ class ChatUI : UI() {
         askForUserName()
     }
 
-    /**
-     * creates the input controls to enter the messages to send and the button to submit the send.
-     */
     private fun createInputs(): Component {
         return HorizontalLayout().apply {
             setWidth(100F, Sizeable.Unit.PERCENTAGE)
@@ -52,9 +46,6 @@ class ChatUI : UI() {
         }
     }
 
-    /**
-     * shows a modal dialog to ask for the user user and sets the correspondig field.
-     */
     private fun askForUserName() {
         addWindow(Window("your user:").apply {
             isModal = true
@@ -78,28 +69,39 @@ class ChatUI : UI() {
         })
     }
 
-    /**
-     * sends the message from the input text field to the backend.
-     */
     private fun sendMessage(message: String) {
         log.warn("{} sending message \"{}\"", user, message)
         showMessage(user, message)
     }
 
-    /**
-     * adds the received message to the display.
-     * @param user the user who sent the message
-     * @param message the message from the user
-     */
     private fun showMessage(user: String, message: String) {
         log.info("got message \"{}\" from {}", message, user)
         chatDisplay.addMessage(user, message)
     }
 
-    /**
-     * companion object containing the logger.
-     */
     companion object {
         val log: Logger = LoggerFactory.getLogger(ChatUI::class.java)
+    }
+}
+
+class ChatDisplay : Panel() {
+    val text: Label
+
+    init {
+        setSizeFull()
+        text = Label().apply {
+            contentMode = ContentMode.HTML
+        }
+        content = VerticalLayout().apply {
+            addComponent(text)
+        }
+    }
+
+    fun addMessage(user: String, message: String) {
+        text.value = when {
+            text.value.isNullOrEmpty() -> "<em>$user:</em> $message"
+            else -> text.value + "<br/><em>$user:</em> $message"
+        }
+        scrollTop = Int.MAX_VALUE
     }
 }
