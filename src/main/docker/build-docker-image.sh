a19bbda6e91778569931f49434c142b9c26b0b74
P.J.Meisch <pj.meisch@sothawo.com>
2017-07-06 11:27:52 +0200

    cleaned up script

--- src/main/docker/build-docker-image.sh
+++ src/main/docker/build-docker-image.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 VERSION=@project.version@-@buildNumber@
-HLX_IMAGE_NAME=@project.name@
+IMAGE_NAME=@project.name@
 
 docker build \
-    -t ${HLX_IMAGE_NAME}:${VERSION} \
-    -t ${HLX_IMAGE_NAME}:latest \
+    -t ${IMAGE_NAME}:${VERSION} \
+    -t ${IMAGE_NAME}:latest \
     .
